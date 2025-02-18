ade60a131e959f1c6295c1b10ded5dc926b44ef9
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2025-02-18 14:12:48 +0000

    chore: print value in received message

--- app/messaging/message-processor.js
+++ app/messaging/message-processor.js
@@ -1,5 +1,7 @@
+import * as util from 'util'
+
 export async function processMessage (logger, message, messageReceiver) {
   // For now we just print out receipt of message
-  logger.info(`Message received ${JSON.stringify(message)}`)
+  logger.info(`Message received ${util.inspect(message.body)}`)
   await messageReceiver.completeMessage(message)
 }
