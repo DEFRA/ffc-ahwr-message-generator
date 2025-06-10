3f4b6c478380ee1c3189b7392e27a193d6f3c25c
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-06-10 14:17:13 +0100

    reverted build schema

--- app/messaging/validate-inbound-message.js
+++ app/messaging/validate-inbound-message.js
@@ -9,28 +9,23 @@ const REFERENCE_LENGTH = 14
 const crn = joi.number().min(CRN_MIN_VALUE).max(CRN_MAX_VALUE)
 const sbi = joi.number().min(SBI_MIN_VALUE).max(SBI_MAX_VALUE).required()
 
-export const buildInboundStatusMessageSchema = () => {
-  const schema = {
-    crn,
-    sbi,
-    agreementReference: joi.string().required().length(REFERENCE_LENGTH),
-    claimReference: joi.string().required().length(REFERENCE_LENGTH),
-    claimStatus: joi.number().required(),
-    claimType: joi.string().required(),
-    typeOfLivestock: joi.string().required(),
-    dateTime: joi.date().iso().required(),
-    reviewTestResults: joi.string().optional(),
-    piHuntRecommended: joi.string().optional(),
-    piHuntAllAnimals: joi.string().optional(),
-    herdName: joi.string().required()
-  }
-
-  return joi.object(schema)
-}
+const inboundStatusMessageSchema = joi.object({
+  crn,
+  sbi,
+  agreementReference: joi.string().required().length(REFERENCE_LENGTH),
+  claimReference: joi.string().required().length(REFERENCE_LENGTH),
+  claimStatus: joi.number().required(),
+  claimType: joi.string().required(),
+  typeOfLivestock: joi.string().required(),
+  dateTime: joi.date().iso().required(),
+  reviewTestResults: joi.string().optional(),
+  piHuntRecommended: joi.string().optional(),
+  piHuntAllAnimals: joi.string().optional(),
+  herdName: joi.string().required()
+})
 
 export const validateStatusMessageRequest = (logger, event) => {
-  const schema = buildInboundStatusMessageSchema()
-  const { error } = schema.validate(event, { abortEarly: false })
+  const { error } = inboundStatusMessageSchema.validate(event, { abortEarly: false })
   if (error) {
     logger.setBindings({ validationError: { details: error.details } })
     return false
